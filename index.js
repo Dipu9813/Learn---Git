bdd3b5252abdf9beb553ba06e927652f42df298a
Dipu9813 <dipruc06@gmail.com>
2025-01-19 09:31:49 +0545

    calculator created

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-console.log("hello dipu");
-console.log('I am noob');
+// console.log("hello dipu");
+// console.log('I am noob');
+calculator=(x,y,op)=>{
+    switch(op){
+        case '+':
+            return x+y;
+        case '-':
+            return x-y;
+        case '*':
+            return x*y;
+        case '/':
+            return x/y;
+        default:
+            console.log('enter valid operator');
+
+    }
+}
+
+result = calculator(2,3,'*');
+console.log(calculator(2,3,'*'));
+console.log(calculator(10,2,'/'));
+
+

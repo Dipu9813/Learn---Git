065ea69034e6443f0856efa3cf19d984a7ed89f1
Dipu9813 <dipruc06@gmail.com>
2025-01-21 10:47:05 +0545

    Day 5- class

---
+++ day5.js
@@ -0,0 +1,37 @@
+
+// console.log('hi')
+// function longTask() {
+//     for (let i=0;i<1e9;i++){
+
+//     }
+//     console.log("long task completed");
+// }
+// longTask();
+// console.log("end");
+
+
+//my doubt about it
+// console.log('first');
+// setTimeout(display,2000);
+// console.log("last");
+
+// function display(){
+//     let a = print(5);
+// }
+// function print(a){
+//     console.log("timeout inside fucntion",a);
+
+// }
+
+//async/await
+
+
+
+
+
+//builtin module of node JS
+// 1. Path module
+// const path = require('path');
+
+// const filePath = '/home/user/project/index.js';
+// console.log()
